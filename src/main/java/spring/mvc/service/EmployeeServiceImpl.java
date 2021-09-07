c0f533fddd1b58e7947d500dedfa58759f209261
Ekaterina Kasatikova <KatyKat2010@yandex.ru>
2021-09-07 04:52:21 +0300

    Added project Spring MVC + Hibernate + AOP

---
+++ src/main/java/spring/mvc/service/EmployeeServiceImpl.java
@@ -0,0 +1,40 @@
+package spring.mvc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import spring.mvc.dao.EmployeeDAO;
+import spring.mvc.entity.Employee;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+    @Override
+    @Transactional
+    public List<Employee> getAllEmployees() {
+        return employeeDAO.getAllEmployees();
+    }
+
+    @Override
+    @Transactional
+    public void saveEmployee(Employee employee) {
+        employeeDAO.saveEmployee(employee);
+    }
+
+    @Override
+    @Transactional
+    public Employee getEmployee(int id) {
+        return employeeDAO.getEmployee(id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteEmployee(int id) {
+        employeeDAO.deleteEmployee(id);
+    }
+}
